b0395e8bbcf8d5018e272acf05ebae560c6f4b8d
Monte Mishkin <montemishkin@gmail.com>
2015-07-25 09:01:22 +0000

    seems to work over local network, curl from server, and start up on server boot

--- mishkin_com/settings/live.py
+++ mishkin_com/settings/live.py
@@ -10,16 +10,19 @@ DEBUG = False
 
 # List of allowed hostnames
 # https://docs.djangoproject.com/en/1.8/ref/settings/#std:setting-ALLOWED_HOSTS
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '10.0.0.29',
+]
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'NAME': 'mishkin_com',
+        'USER': 'mishkin_com',
+        'PASSWORD': 'monte',
         'HOST': 'localhost',
         'PORT': '',
     }
