79c736f4d3f497b9cf70b4818b4c8be12c943701
Monte Mishkin <montemishkin@gmail.com>
2015-07-26 02:44:17 -0700

    working example with less agressive humor

--- mishkin_com/assets/scripts/components/Root/index.js
+++ mishkin_com/assets/scripts/components/Root/index.js
@@ -15,48 +15,45 @@ class Root extends React.Component {
 
         // set initial state
         this.state = {
-            mouse_is_down: false,
-            has_been_clicked: false,
-            button_is_hovered: false,
+            expanded: false,
+            button_focus: false,
+            button_hover: false,
         }
     }
 
 
     // render component
     render() {
-        let dynamic_content = (
+        return (<div style={styles.container}>
+            <p style={styles.big_text}>
+                The Mishkins
+            </p>
+            <p style={styles.small_text}>
+                more than just a family
+            </p>
+            <p styles={[
+                styles.small_text,
+                this.state.expanded && styles.expand,
+                !this.state.expanded && styles.contract,
+            ]}>
+                The Mishkins are a family, but they are also more.
+            </p>
             <button
                 type='button'
                 styles={[
                     styles.button,
-                    this.state.button_is_hovered && styles.button_hover,
-                    this.state.mouse_is_down && styles.button_mouse_down,
+                    this.state.button_focus && styles.button_focus,
+                    this.state.button_hover && styles.button_hover,
                 ]}
-                onClick={() => this.setState({has_been_clicked: true})}
-                onMouseEnter={() => this.setState({button_is_hovered: true})}
-                onMouseLeave={() => this.setState({button_is_hovered: false})}
-                onMouseDown={() => this.setState({mouse_is_down: true})}
-                onMouseUp={() => this.setState({mouse_is_down: false})}
-            >
-                Learn More
-            </button>
-        )
-
-        if (this.state.has_been_clicked) {
-            dynamic_content = (
-                <p style={styles.small_text}>Seriously.</p>
-            )
-        }
-
-
-        return (<div style={styles.container}>
-            <p style={styles.big_text}>
-                {"Don't mess with"}
-            </p>
-            <p style={styles.big_text}>
-                The Mishkins.
-            </p>
-            {dynamic_content}
+                onClick={() => this.setState({expanded: !this.state.expanded})}
+                onFocus={() => this.setState({button_focus: true})}
+                onBlur={() => this.setState({button_focus: false})}
+                onMouseEnter={() => this.setState({button_hover: true})}
+                onMouseLeave={() => this.setState({button_hover: false})}
+            >{[
+                'Learn More',
+                'Learn Less',
+            ][+this.state.expanded]}</button>
         </div>)
     }
 }
--- mishkin_com/assets/scripts/components/Root/styles.js
+++ mishkin_com/assets/scripts/components/Root/styles.js
@@ -9,6 +9,15 @@ import colors from '../../../styles/colors'
 import black_paper from '../../../images/black_paper.png'
 
 
+/* constants */
+const big_margin = 55
+const small_margin = 20
+const padding = 20
+const big_font_size = 80
+const small_font_size = 20
+const animation_params = '0.2s ease-in-out'
+
+
 // define the style sheet
 let styles = StyleSheet.create({
     container: {
@@ -28,33 +37,48 @@ let styles = StyleSheet.create({
 
     big_text: {
         display: 'inline',
-        margin: 20,
-        fontSize: 80,
+        margin: small_margin,
+        fontSize: big_font_size,
     },
 
     small_text: {
-        margin: 55,
-        padding: 20,
-        fontSize: 20,
+        margin: small_margin,
+        padding: padding,
+        fontSize: small_font_size,
+    },
+
+    expand: {
+        opacity: 1,
+        transition: `all ${animation_params}`,
+    },
+
+    contract: {
+        opacity: 0,
+        height: 0,
+        margin: 0,
+        border: 0,
+        padding: 0,
+        transition: `all ${animation_params}`,
     },
 
     button: {
-        margin: 55,
+        margin: small_margin,
         border: 0,
         borderRadius: 10,
-        padding: 20,
-        fontSize: 20,
+        outline: 0,
+        padding: padding,
+        fontSize: small_font_size,
         backgroundColor: colors.blue.darker,
-        transition: 'background-color 0.2s ease-in-out',
+        transition: `background-color ${animation_params}`,
+    },
+
+    button_focus: {
+        outline: 0,
     },
 
     button_hover: {
         backgroundColor: colors.blue.main,
-        transition: 'background-color 0.2s ease-in-out',
-    },
-
-    button_mouse_down: {
-        outline: 0,
+        transition: `background-color ${animation_params}`,
     },
 })
 
